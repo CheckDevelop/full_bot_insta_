24536f1e270ea797127bc495eb6022e4ec2d4745
CheckDevelop <166996908+CheckDevelop@users.noreply.github.com>
2026-08-16 19:51:16 +0330

    Update instagram.py

--- app/instagram.py
+++ app/instagram.py
@@ -1054,9 +1054,7 @@ class InstagramClient:
 
 
         html = response.text
-        matches = re.findall(r'"USER_ID"\s*:\s*"([^"]*)"', html)
-        for i, user_id in enumerate(matches, 1):
-            print(i, user_id)
+        print(html)
 
         if not html:
 
