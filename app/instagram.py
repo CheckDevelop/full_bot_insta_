4e25d5cbfd7060aa29c5e04ce2b7b4897f2ce2b5
CheckDevelop <166996908+CheckDevelop@users.noreply.github.com>
2026-08-16 18:51:55 +0330

    Update instagram.py

--- app/instagram.py
+++ app/instagram.py
@@ -969,6 +969,30 @@ class InstagramClient:
     # ============================================================
     # Normal Story
     # ============================================================
+    def get_instagram_user_id(username):
+        try:
+            # ساختن instance بدون دانلود پست‌ها
+            L = instaloader.Instaloader(download_pictures=False, 
+                                         download_videos=False,
+                                         download_comments=False,
+                                         save_metadata=False)
+            
+            # گرفتن پروفایل
+            profile = instaloader.Profile.from_username(L.context, username)
+            
+            return {
+                'user_id': profile.userid,
+                'username': profile.username,
+                'full_name': profile.full_name,
+                'followers': profile.followers,
+                'following': profile.followees,
+                'is_private': profile.is_private,
+                'is_verified': profile.is_verified
+            }
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
 
     def _get_story_owner_id_from_html(
         self,
@@ -1098,7 +1122,8 @@ class InstagramClient:
         session = (
             self.loader.context._session
         )
-
+        result = get_instagram_user_id("shiraztagram")
+        print("user id: ", result)
         csrf_token = self._get_cookie(
             "csrftoken"
         )
